6163f5536ab669394152ddd67139ef1181ee32d4
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-28 09:42:29 +0900

    request from client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const fetchDeals = async () => {
+  const res = await fetch("/api/deals");
+  const data = await res.json();
+  console.log(data);
+};
+
+const App = () => {
+  useEffect(() => {
+    fetchDeals();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +30,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
