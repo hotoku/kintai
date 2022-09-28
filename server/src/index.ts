b8d4a790fba568cd0253d8975c8cd8f02968e50a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-28 14:14:08 +0900

    [db] work_hours

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,23 @@ from
 `);
     res.send(ret);
   });
+  app.use("/api/work_hours", async (req, res) => {
+    const db = getInstance();
+    db.open();
+    const deal_id = (req.query as any).deal_id;
+    const ret = await db.all(`
+select
+  id,
+  deal_id,
+  start_time,
+  end_time
+from
+  work_hours
+where
+  deal_id=${deal_id}
+`);
+    res.send(ret);
+  });
 
   app.listen(8080);
 };
