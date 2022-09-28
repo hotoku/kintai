331e9fe995c1c6ef2060e3986446e5f1a6164f33
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-28 09:10:41 +0900

    db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
 
+import { getInstance } from "./db/db";
+
 const start = () => {
   const app = express();
   app.use("/api/deals", () => {
-    console.log("deals");
+    const db = getInstance();
+    db.open();
+    const ret = db.get(`
+select
+  id,
+  name
+from
+  deals
+`);
+    console.log(ret);
   });
 
   app.listen(8080);
