e3df8e1c27ce9a61ad85f59cdc0acd5f3cefe570
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-09-29 16:57:47 +0900

    compiles but not work

--- client/src/components/WorkHours.tsx
+++ client/src/components/WorkHours.tsx
@@ -6,17 +6,10 @@ import { WorkHour } from "../api/types";
 import { parseQuery } from "../utils";
 
 type HalfwayWorkHour = {
+  id?: number;
   dealId: number;
   startTime?: string;
   endTime?: string;
-};
-
-const createItem = (wh: WorkHour): JSX.Element => {
-  return (
-    <li key={wh.id}>
-      {wh.startTime}, {wh.endTime || "null"}
-    </li>
-  );
 };
 
 interface IEditorProps {
@@ -25,6 +18,21 @@ interface IEditorProps {
   onSave: (obj: HalfwayWorkHour) => void;
   onCancel: (obj: HalfwayWorkHour) => void;
 }
+
+interface IViewProps {
+  currentObj: HalfwayWorkHour;
+  onEdit: (obj: HalfwayWorkHour) => void;
+}
+
+type ViewOrEditorProps = IEditorProps & IViewProps;
+
+const View = ({ currentObj, onEdit }: IViewProps) => {
+  return (
+    <span>
+      {currentObj.startTime}, {currentObj.endTime || "null"}
+    </span>
+  );
+};
 
 const Editor = ({ currentObj, onChange, onSave, onCancel }: IEditorProps) => {
   const handleChange =
@@ -55,6 +63,15 @@ const Editor = ({ currentObj, onChange, onSave, onCancel }: IEditorProps) => {
   );
 };
 
+const createViewOrEditor = (
+  editedId: number | "new" | undefined,
+  props: ViewOrEditorProps
+): JSX.Element => {
+  const wh = props.currentObj;
+  const ret = wh.id === editedId ? <Editor {...props} /> : <View {...props} />;
+  return <li key={wh.id}>{ret}</li>;
+};
+
 const WorkHours = () => {
   const query = parseQuery(useLocation().search);
   if (query["dealId"] === undefined) {
@@ -66,17 +83,25 @@ const WorkHours = () => {
   }
 
   const [workHours, setWorkHours] = useState<WorkHour[]>([]);
-  const [addingRecord, setAddingRecord] = useState<HalfwayWorkHour>({
+  const [editedRecord, setEditedRecord] = useState<HalfwayWorkHour>({
     dealId: dealId,
   });
-  const [isAdding, setIsAdding] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editedId, setEditedId] = useState<number | "new" | undefined>();
 
   useEffect(() => {
     fetchWorkHours(dealId, setWorkHours);
   }, [dealId]);
 
   const handleAddClick = () => {
-    setIsAdding(true);
+    setIsEditing(true);
+    setEditedId("new");
+  };
+
+  const handleEditClick = (wh: HalfwayWorkHour) => {
+    const id = wh.id;
+    setEditedId(id);
+    setIsEditing(true);
   };
 
   const handleSave = async (wh: HalfwayWorkHour) => {
@@ -84,26 +109,49 @@ const WorkHours = () => {
     const obj: WorkHour = { ...wh, startTime: wh.startTime };
     await postWorkHours(obj);
     fetchWorkHours(dealId, setWorkHours);
-    setIsAdding(false);
-    setAddingRecord({ dealId: dealId });
+    setIsEditing(false);
+    setEditedRecord({ dealId: dealId });
   };
 
-  const items = workHours.map(createItem);
-  if (isAdding) {
+  const handleCancel = (_: any) => {
+    setEditedRecord({ dealId: dealId });
+    setIsEditing(false);
+  };
+
+  const items = workHours.map((wh) => {
+    return createViewOrEditor(editedId, {
+      currentObj: editedRecord,
+      onChange: (wh) => setEditedRecord(wh),
+      onSave: handleSave,
+      onCancel: handleCancel,
+      onEdit: handleEditClick,
+    });
+  });
+
+  if (isEditing) {
     items.push(
-      <li key="editor">
+      <li key="new">
         <Editor
-          currentObj={addingRecord}
-          onChange={setAddingRecord}
+          currentObj={editedRecord}
+          onChange={setEditedRecord}
           onSave={handleSave}
-          onCancel={() => setIsAdding(false)}
+          onCancel={handleCancel}
         />
       </li>
     );
   }
 
   return (
-    <div className="WorkHours">
+    <div
+      className="WorkHours"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        console.log(e.code);
+        if (e.code == "KeyA") {
+          handleAddClick();
+        }
+      }}
+    >
       <button onClick={handleAddClick}>add</button>
       <ul>{items}</ul>
     </div>
