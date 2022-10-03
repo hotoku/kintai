0ad94aded3c57e0c9b0ed030432c5cd9a454458f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-04 00:12:47 +0900

    wip: this compiles ...
    When utils.ts is put as a symbolic link, compile fails.
    When utils.ts is copied, compiole succeded.

--- src/components/WorkHours.tsx
+++ src/components/WorkHours.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
-import { formatDate, formatTime } from "./utils";
+import { formatDate, formatTime } from "../share/utils";
 
 import { fetchWorkHours, postWorkHour, putWorkHour } from "../api/fetches";
 import { WorkHour, HalfwayWorkHour } from "../api/types";
