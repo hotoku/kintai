724dc29666b238591b3b999bee1f57eeeb8ef0a4
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-04 18:41:12 +0900

    db migration for release

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -2,7 +2,9 @@ import { Database } from "sqlite";
 import { Database as Driver } from "sqlite3";
 
 export const getInstance = (): Database => {
-  const db_path = `${process.cwd()}/db/db.sqlite`;
+  const db_dir =
+    `${process.env.KINTAI_ONEDRIVE_PATH}/db` || `${process.cwd()}/db`;
+  const db_path = db_dir + `/db.sqlite`;
   const db = new Database({
     filename: db_path,
     driver: Driver,
