969e56a52ab76108a32ce1ee712ce50038d3ef8c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-05 08:04:54 +0900

    log access

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 
 import { clients, deals, staticFiles, workHours } from "./server/routes";
 
 const start = () => {
   const app = express();
   app.use(bodyParser.json());
+  app.use(morgan("combined"));
+
   deals(app);
   workHours(app);
   clients(app);
   staticFiles(app);
+
   app.listen(8080);
 };
 
