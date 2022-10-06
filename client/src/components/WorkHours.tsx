95c859965a10cc37352d6fef7ecf0595c55dbaca
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-06 09:42:39 +0900

    tiny

--- client/src/components/WorkHours.tsx
+++ client/src/components/WorkHours.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DateTimePicker from "react-datetime-picker";
 import "react-datetime-picker/dist/DateTimePicker.css";
