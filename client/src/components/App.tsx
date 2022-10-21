9be90d9cca1300248a5a84a1284c487ab7a55183
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-21 16:51:22 +0900

    tiny

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Clients from "./Clients";
 import Deals from "./Deals";
 import Home from "./Home";
