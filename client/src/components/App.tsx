8ca663a5db941b52d6ce90ef3b9f4d6232cc97ad
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-21 16:46:44 +0900

    add deleted work hours

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,6 +7,7 @@ import WorkHours from "./WorkHours";
 import Style from "./App.module.css";
 import Header from "./Header";
 import ErrorBoundary from "./ErrorBoundary";
+import DeletedWorkHours from "./DeletedWorkHours";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
               <Route path="/clients" element={<Clients />} />
               <Route path="/workHours" element={<WorkHours />} />
               <Route path="/deals" element={<Deals />} />
+              <Route path="/deletedWorkHours" element={<DeletedWorkHours />} />
             </Routes>
           </ErrorBoundary>
         </div>
---
+++ client/src/components/DeletedWorkHours.tsx
@@ -0,0 +1,5 @@
+const DeletedWorkHours = (): JSX.Element => {
+  return <div />;
+};
+
+export default DeletedWorkHours;
