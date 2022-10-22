0a05370df949b6ce433592e82ad46c004b8b7e7e
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-23 08:55:46 +0900

    fk for work hours

--- server/migrations/006-add_fk_constraint__work_hours.sql
+++ server/migrations/006-add_fk_constraint__work_hours.sql
@@ -1,8 +1,20 @@
 --------------------------------------------------------------------------------
 -- Up
 --------------------------------------------------------------------------------
-alter table WorkHours
-  add isDeleted integer default FALSE;
+alter table WorkHours rename to _WorkHours_old;
+
+create table WorkHours (
+       id integer primary key,
+       dealId integer not null,
+       startTime text not null,
+       endTime text,
+       foreign key(dealId) references Deals(id)
+);
+
+insert into WorkHours (id, dealId, startTime, endTime)
+  select id, dealId, startTime, endTime from _WorkHours_old;
+
+drop table _WorkHours_old;
 
 --------------------------------------------------------------------------------
 -- Down
