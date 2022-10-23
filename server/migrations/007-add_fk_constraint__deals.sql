0212dbe1b846c3f603e87d85cc5bb0feba870534
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-23 16:59:58 +0900

    fk deals

---
+++ server/migrations/007-add_fk_constraint__deals.sql
@@ -0,0 +1,36 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+alter table Deals rename to _Deals_old;
+
+
+CREATE TABLE Deals
+(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  clientId integer not null,
+  foreign key(clientId) references Clients(id)
+)
+
+insert into Deals (id, name, clientId)
+  select id, name, clientId from _Deals_old;
+
+drop table _Deals_old;
+
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
+ALTER TABLE Deals RENAME TO _Deals_old;
+
+CREATE TABLE Deals
+(
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  clientId integer not null
+)
+
+INSERT INTO Deals (id, name, clientId)
+  SELECT id, name, clientId
+  FROM _Deals_old;
+
+drop table _Deals_old;
