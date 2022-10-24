d44c7c9e985c65ef5471eed98fa4c99f990b6b79
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-24 17:06:32 +0900

    port and path has no default values

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -3,16 +3,18 @@
  * 開発時には、カレントディレクトリを返す。
  */
 export const sync_data_path = (): string => {
-  return process.env.KINTAI_SYNC_DATA_PATH || process.cwd();
+  if (!process.env.KINTAI_SYNC_DATA_PATH) {
+    throw "KINTAI_SYNC_DATA_PATH is not defined";
+  }
+  return process.env.KINTAI_SYNC_DATA_PATH;
 };
 
 /**
  * WEBサーバーの待受ポート
  */
 export const port = (): number => {
-  if (process.env.KINTAI_PORT) {
-    return parseInt(process.env.KINTAI_PORT);
-  } else {
-    return 8080;
+  if (!process.env.KINTAI_PORT) {
+    throw "KINTAI_PORT is not defined";
   }
+  return parseInt(process.env.KINTAI_PORT);
 };
