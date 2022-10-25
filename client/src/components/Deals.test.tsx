c1361b99c00ba9edbb481b14b4c87dfabf60b9e4
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-25 15:44:16 +0900

    wip

--- client/src/components/Deals.test.tsx
+++ client/src/components/Deals.test.tsx
@@ -1,15 +1,51 @@
-import { render, screen } from "@testing-library/react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
 import Deals from "./Deals";
 
-test("renders table", () => {
-  const ret = render(<Deals clientId={1} />);
-  const table = ret.container.querySelector("table");
-  expect(table).not.toBeNull();
-  if (table === null) throw "panic";
-  const head = table.getElementsByTagName("thead");
-  expect(head.length).toBe(1);
-  const body = table.getElementsByTagName("tbody");
-  expect(body.length).toBe(1);
-  const rows = body[0].getElementsByTagName("tr");
-  expect(rows.length).toBe(1);
+let container: any = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders user data", async () => {
+  const makeDeal = (data: [number, string, number]) => {
+    return { id: data[0], name: data[1], clientId: data[2] };
+  };
+
+  const fakeDeals = (
+    [
+      [1, "1", 1],
+      [2, "2", 1],
+      [3, "3", 1],
+      [4, "4", 2],
+    ] as [number, string, number][]
+  ).map(makeDeal);
+
+  const spy = jest.spyOn(global, "fetch");
+  spy.mockImplementation(
+    jest.fn(() =>
+      Promise.resolve({ json: () => Promise.resolve(fakeDeals) })
+    ) as jest.Mock
+  );
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<Deals clientId={1} />, container);
+  });
+
+  expect(container.querySelector("summary").textContent).toBe(fakeUser.name);
+  expect(container.querySelector("strong").textContent).toBe(fakeUser.age);
+  expect(container.textContent).toContain(fakeUser.address);
+
+  // remove the mock to ensure tests are completely isolated
+  spy.mockRestore();
 });
