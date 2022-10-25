60fbfe90e71acfb8d7774a589f5b495e0bfdaa90
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-10-25 14:58:30 +0900

    Deals test. We need to know how to mock fetch.

--- client/src/components/Deals.test.tsx
+++ client/src/components/Deals.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import Deals from "./Deals";
 
-test("renders learn react link", () => {
+test("renders table", () => {
   const ret = render(<Deals clientId={1} />);
   const table = ret.container.querySelector("table");
   expect(table).not.toBeNull();
+  if (table === null) throw "panic";
+  const head = table.getElementsByTagName("thead");
+  expect(head.length).toBe(1);
+  const body = table.getElementsByTagName("tbody");
+  expect(body.length).toBe(1);
+  const rows = body[0].getElementsByTagName("tr");
+  expect(rows.length).toBe(1);
 });
