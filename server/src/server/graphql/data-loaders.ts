3fa5317c0d3eb74eb1012ba2467137faed2aad6f
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-22 17:02:50 +0900

    this works
    ```
    {
      getClient(id: 1) {
        id
        name
      }
    }
    ```

--- server/src/server/graphql/data-loaders.ts
+++ server/src/server/graphql/data-loaders.ts
@@ -1,8 +1,23 @@
+import DataLoader from "dataloader";
+
 export type ClientRecord = {
   id: number;
   name: string;
 };
 
+export function createClientLoader(): DataLoader<number, ClientRecord> {
+  return new DataLoader<number, ClientRecord>(async (ids) => {
+    const ret = [] as ClientRecord[];
+    for (const id of ids) {
+      ret.push({
+        id: id,
+        name: `client-${id}`,
+      });
+    }
+    return ret;
+  });
+}
+
 export type MyDataLoader = {
-  clientLoader: { load: (id: number) => {} };
+  clientLoader: DataLoader<number, ClientRecord>;
 };
--- server/src/server/graphql/index.ts
+++ server/src/server/graphql/index.ts
@@ -3,12 +3,17 @@ import { graphqlHTTP } from "express-graphql";
 import { GraphQLSchema } from "graphql";
 
 import { queryType } from "./resolvers";
+import { createClientLoader, MyDataLoader } from "./data-loaders";
 
 function graphql(app: Application) {
   app.use("/graphql", (req, res) => {
+    const loader: MyDataLoader = {
+      clientLoader: createClientLoader(),
+    };
     return graphqlHTTP({
       schema: new GraphQLSchema({ query: queryType }),
       graphiql: true,
+      context: { loader },
     })(req, res);
   });
 }
