acfce550f5eed4131714638b7428b26bc5333f82
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-29 16:50:36 +0900

    wip

--- server/src/server/graphql/resolvers/mutation.ts
+++ server/src/server/graphql/resolvers/mutation.ts
@@ -1,18 +1,48 @@
 import {
-  GraphQLList,
   GraphQLInt,
   GraphQLNonNull,
   GraphQLString,
   GraphQLObjectType,
+  GraphQLBoolean,
 } from "graphql";
 import { getPool } from "../../../db/db";
+import { WorkHourType } from "../objectTypes";
 
-import { ClientType, DealType, WorkHourType } from "../objectTypes";
 import { ContextType } from "./index";
 
 export const mutationType = new GraphQLObjectType<{}, ContextType>({
   name: "Mutation",
   fields: {
+    updateWorkHour: {
+      type: WorkHourType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+        startTime: { type: new GraphQLNonNull(GraphQLString) },
+        endTime: { type: GraphQLString },
+        isDeleted: { type: new GraphQLNonNull(GraphQLBoolean) },
+        dealId: { type: GraphQLInt },
+        note: { type: GraphQLString },
+      },
+      resolve: async (_, args) => {
+        const db = getPool();
+        const sql = `
+          update WorkHours
+          set
+          startTime = ${args.startTime},
+          endTime = ${args.endTime || "null"}
+          isDeleted = ${args.isDeleted || "null"}
+          
+          where
+            id = ${args.id}
+        `;
+        try {
+          await db.query(sql);
+          return "ok";
+        } catch (e) {
+          return new Error(JSON.stringify(e));
+        }
+      },
+    },
     deleteWorkHour: {
       type: GraphQLString,
       args: {
@@ -25,8 +55,12 @@ export const mutationType = new GraphQLObjectType<{}, ContextType>({
         const sql = `
           delete from WorkHours where id=${args.id}
         `;
-        await db.query(sql);
-        return "ok";
+        try {
+          await db.query(sql);
+          return "ok";
+        } catch (e) {
+          return new Error(JSON.stringify(e));
+        }
       },
     },
   },
