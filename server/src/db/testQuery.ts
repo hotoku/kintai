f5390cb3c8378b5536bbe70104a81a8af5e0dc0b
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2022-11-30 09:28:00 +0900

    query sample

---
+++ server/src/db/testQuery.ts
@@ -0,0 +1,32 @@
+/**
+ * DBへのクエリの検証に使う
+ */
+
+import mysql from "mysql2/promise";
+
+const DB_CONFIG = {
+  host: "127.0.0.1",
+  user: process.env.MYSQL_USER_NAME,
+  database: process.env.DATABASE_NAME,
+  password: process.env.MYSQL_USER_PASSWORD,
+};
+
+async function main() {
+  const con = await mysql.createConnection(DB_CONFIG);
+  const [rows, _] = await con.query(
+    `
+    select
+      id,
+      name
+    from
+      clients
+    where
+      id=?`,
+    [1]
+  );
+  console.log(rows);
+  await con.end();
+  return;
+}
+
+main();
