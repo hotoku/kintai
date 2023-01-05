49a0a6e3347cd515e02065395da3bbd2ed3602d3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-01-05 12:16:21 +0900

    drawer

--- client/src/components/KintaiAppBar.tsx
+++ client/src/components/KintaiAppBar.tsx
@@ -1,15 +1,32 @@
-import { AppBar, IconButton, Link, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Drawer,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 import { blue } from "@mui/material/colors";
+import { useState } from "react";
 
 function KintaiAppBar(): JSX.Element {
+  const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false);
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton size="large">
+        <IconButton size="large" onClick={() => setIsOpenDrawer((x) => !x)}>
           <MenuIcon />
         </IconButton>
+        <Drawer
+          anchor="left"
+          open={isOpenDrawer}
+          onClose={() => setIsOpenDrawer((x) => !x)}
+        >
+          {["a", "b"]}
+        </Drawer>
         <Link href="/">
           <Typography variant="h5" color={blue[50]}>
             Kintai
