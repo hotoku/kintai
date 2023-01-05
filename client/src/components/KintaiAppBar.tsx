48e12f28d0be0466935461ffee5c33b767d9df2c
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-01-05 12:31:59 +0900

    finish drawer

--- client/src/components/KintaiAppBar.tsx
+++ client/src/components/KintaiAppBar.tsx
@@ -3,6 +3,8 @@ import {
   Drawer,
   IconButton,
   Link,
+  List,
+  ListItem,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -10,6 +12,14 @@ import MenuIcon from "@mui/icons-material/Menu";
 
 import { blue } from "@mui/material/colors";
 import { useState } from "react";
+
+type DrawerItemProps = {
+  href: string;
+  text: string;
+};
+function DrawerItem({ href, text }: DrawerItemProps): JSX.Element {
+  return <Link href={href}>{text}</Link>;
+}
 
 function KintaiAppBar(): JSX.Element {
   const [isOpenDrawer, setIsOpenDrawer] = useState<boolean>(false);
@@ -25,7 +35,14 @@ function KintaiAppBar(): JSX.Element {
           open={isOpenDrawer}
           onClose={() => setIsOpenDrawer((x) => !x)}
         >
-          {["a", "b"]}
+          <List>
+            <ListItem>
+              <DrawerItem href="/" text="home" />
+            </ListItem>
+            <ListItem>
+              <DrawerItem href="/deals" text="deals" />
+            </ListItem>
+          </List>
         </Drawer>
         <Link href="/">
           <Typography variant="h5" color={blue[50]}>
