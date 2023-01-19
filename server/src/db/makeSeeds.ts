8f3b09c1ac6622309a321db0753cc4c529981970
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-01-20 08:51:18 +0900

    seeds

--- server/src/db/makeSeeds.ts
+++ server/src/db/makeSeeds.ts
@@ -1,46 +1,62 @@
 import * as fs from "fs";
+import dayjs from "dayjs";
 
-const numClients = 100;
-const numDealsPerClient = 100;
-const numWorkHoursPerDeal = 200;
+const numClients = 10;
+const numDealsPerClient = 10;
+const numWorkHoursPerDeal = 20;
 
 const clients = ["id,name"];
 const deals = ["id,name,clientId"];
 const workHours = ["id,dealId,startTime,endTime,isDeleted,note"];
 
-let clientId = 1;
-let dealId = 1;
-let workHourId = 1;
-
-function insertClient() {
+function insertClient(clientId: number) {
   clients.push(`${clientId},client-${clientId}`);
 }
 
-function insertDeal() {
+function insertDeal(clientId: number, dealId: number) {
   deals.push(`${dealId},client-${clientId}/deal-${dealId},${clientId}`);
 }
 
-function insertWorkHour() {
-  workHours.push(`${workHourId}`);
+function insertWorkHour(
+  dealId: number,
+  workHourId: number,
+  start: dayjs.Dayjs
+) {
+  const end = start.add(1, "hour");
+  workHours.push(
+    `${workHourId},${dealId},${start.toISOString()},${end.toISOString()},0,note`
+  );
 }
 
-while (clientId <= numClients) {
-  insertClient();
-  let numDeal = 0;
-  while (numDeal < numDealsPerClient) {
-    insertDeal();
-    let numWorkHour = 0;
-    while (numWorkHour < numWorkHoursPerDeal) {
-      insertWorkHour();
-      numWorkHour++;
-      workHourId++;
+function main() {
+  let clientId = 1;
+  let dealId = 1;
+  let workHourId = 1;
+  let start = dayjs("2022-01-01T09:00:00");
+
+  while (clientId <= numClients) {
+    insertClient(clientId);
+    let numDeal = 0;
+    while (numDeal < numDealsPerClient) {
+      insertDeal(clientId, dealId);
+      let numWorkHour = 0;
+      while (numWorkHour < numWorkHoursPerDeal) {
+        insertWorkHour(dealId, workHourId, start);
+        numWorkHour++;
+        workHourId++;
+        start = start.add(1, "hour");
+        if (start.hour() > 18) {
+          start = start.add(1, "day").hour(9);
+        }
+      }
+      numDeal++;
+      dealId++;
     }
-    numDeal++;
-    dealId++;
+    clientId++;
   }
-  clientId++;
-}
 
-fs.writeFileSync("seeds/clients.csv", clients.join("\n") + "\n");
-fs.writeFileSync("seeds/deals.csv", deals.join("\n") + "\n");
-fs.writeFileSync("seeds/workHours.csv", workHours.join("\n") + "\n");
+  fs.writeFileSync("seeds/clients.csv", clients.join("\n") + "\n");
+  fs.writeFileSync("seeds/deals.csv", deals.join("\n") + "\n");
+  fs.writeFileSync("seeds/workHours.csv", workHours.join("\n") + "\n");
+}
+main();
