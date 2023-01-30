7530d24566f10a33ea8c0a7b38b9956a77890d86
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-01-31 06:25:58 +0900

    content

---
+++ client/src/components/WeekPage/Content.tsx
@@ -0,0 +1,79 @@
+import {
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { formatTime } from "../../share/utils";
+import { formatDate, secToStr } from "../utils";
+import { DaySummary, WorkHour } from "./utils";
+
+function duration(wh: WorkHour): number {
+  if (!wh.endTime) return 0;
+  return (wh.endTime.getTime() - wh.startTime.getTime()) / 1000;
+}
+
+function RenderWorkHour({ wh }: { wh: WorkHour }): JSX.Element {
+  return (
+    <TableRow>
+      <TableCell style={{ width: "6rem" }}>
+        {formatTime(wh.startTime)} - {wh.endTime ? formatTime(wh.endTime) : ""}
+      </TableCell>
+      <TableCell style={{ width: "3rem" }}>
+        {wh.endTime ? `${secToStr(duration(wh))}` : "-"}
+      </TableCell>
+      <TableCell style={{ width: "15rem" }}>{wh.deal.client.name}</TableCell>
+      <TableCell style={{ width: "15rem" }}>
+        {wh.deal.name.substring(0, 20)}
+      </TableCell>
+      <TableCell style={{ width: "15rem" }}>{wh.note}</TableCell>
+    </TableRow>
+  );
+}
+
+function RenderDaySummary({ ds }: { ds: DaySummary }): JSX.Element {
+  const totalDuration = secToStr(
+    ds.workHours.map((wh) => duration(wh)).reduce((x, y) => x + y, 0)
+  );
+  return (
+    <Card
+      style={{
+        margin: "10px",
+        background: "#f7f7f7",
+        padding: "5px 5px 0",
+      }}
+    >
+      <Typography component="div" variant="h6">
+        {formatDate(ds.date)}: 合計 {totalDuration}
+      </Typography>
+      <TableContainer>
+        <Table>
+          <TableBody>
+            {ds.workHours
+              .filter((wh) => !wh.isDeleted)
+              .map((wh) => (
+                <RenderWorkHour key={wh.id} wh={wh} />
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  );
+}
+
+type ContentProps = {
+  summaries: DaySummary[];
+};
+function Content({ summaries }: ContentProps): JSX.Element {
+  return (
+    <>
+      {summaries.map((s) => (
+        <RenderDaySummary key={s.date.toISOString()} ds={s} />
+      ))}
+    </>
+  );
+}
+export default Content;
--- client/src/components/WeekPage/index.tsx
+++ client/src/components/WeekPage/index.tsx
@@ -16,63 +16,11 @@ import {
 import { ArrowForward, ArrowBack } from "@mui/icons-material";
 import { useLocation, useNavigate } from "react-router-dom";
 import dayjs from "dayjs";
-
-function duration(wh: WorkHour): number {
-  if (!wh.endTime) return 0;
-  return (wh.endTime.getTime() - wh.startTime.getTime()) / 1000;
-}
+import Content from "./Content";
 
 type WeekPageProps = {
   date: string;
 };
-
-function RenderWorkHour({ wh }: { wh: WorkHour }): JSX.Element {
-  return (
-    <TableRow>
-      <TableCell style={{ width: "6rem" }}>
-        {formatTime(wh.startTime)} - {wh.endTime ? formatTime(wh.endTime) : ""}
-      </TableCell>
-      <TableCell style={{ width: "3rem" }}>
-        {wh.endTime ? `${secToStr(duration(wh))}` : "-"}
-      </TableCell>
-      <TableCell style={{ width: "15rem" }}>{wh.deal.client.name}</TableCell>
-      <TableCell style={{ width: "15rem" }}>
-        {wh.deal.name.substring(0, 20)}
-      </TableCell>
-      <TableCell style={{ width: "15rem" }}>{wh.note}</TableCell>
-    </TableRow>
-  );
-}
-
-function RenderDaySummary({ ds }: { ds: DaySummary }): JSX.Element {
-  const totalDuration = secToStr(
-    ds.workHours.map((wh) => duration(wh)).reduce((x, y) => x + y, 0)
-  );
-  return (
-    <Card
-      style={{
-        margin: "10px",
-        background: "#f7f7f7",
-        padding: "5px 5px 0",
-      }}
-    >
-      <Typography component="div" variant="h6">
-        {formatDate(ds.date)}: 合計 {totalDuration}
-      </Typography>
-      <TableContainer>
-        <Table>
-          <TableBody>
-            {ds.workHours
-              .filter((wh) => !wh.isDeleted)
-              .map((wh) => (
-                <RenderWorkHour key={wh.id} wh={wh} />
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Card>
-  );
-}
 
 type NavigationProps = {
   onBackClick: () => Promise<void>;
@@ -125,9 +73,7 @@ function WeekPage({ date: date_ }: WeekPageProps): JSX.Element {
         onForwardClick={handleForwardClick}
         onBackClick={handleBackClick}
       />
-      {summaries.map((s) => (
-        <RenderDaySummary key={s.date.toISOString()} ds={s} />
-      ))}
+      <Content summaries={summaries} />
     </Paper>
   );
 }
