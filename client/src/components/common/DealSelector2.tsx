e102b1d88f0bbedbc8c8a7c2ff4a0bebe617ece3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-02-24 17:00:53 +0900

    deal selector 2: compiles

--- client/src/components/common/DealSelector2.tsx
+++ client/src/components/common/DealSelector2.tsx
@@ -1,28 +1,38 @@
-import { Box, MenuItem, Select, SelectChangeEvent } from "@mui/material";
-import { useState } from "react";
-import { maybeInt } from "../../utils";
+import { Box, MenuItem, Select } from "@mui/material";
+import { useMemo, useState } from "react";
 
-export type ClientMap = Map<number, string>;
-export type DealMap = Map<number, string>;
-
-type DealSelectorProps = {
-  clients: ClientMap;
-  deals: DealMap;
-  onClientChange: (id: number | "") => Promise<void>;
-  onDealChange: (id: number | "") => Promise<void>;
+type Deal = {
+  id: number;
+  name: string;
 };
 
-function menuItem(map: Map<number, string>): JSX.Element[] {
+type Client = {
+  id: number;
+  name: string;
+  deals: Deal[];
+};
+
+type Selection = {
+  clientId: number | "";
+  dealId: number | "";
+};
+
+type DealSelectorProps = {
+  clients: Client[];
+  onSelectionChange: (s: Selection) => Promise<void>;
+};
+
+function menuItem(items: { id: number; name: string }[]): JSX.Element[] {
   const ret = [] as JSX.Element[];
   ret.push(
     <MenuItem key={"none"} value="">
       <em>All</em>
     </MenuItem>
   );
-  map.forEach((name, id) => {
+  items.forEach((item) => {
     ret.push(
-      <MenuItem key={id} value={id}>
-        {name}
+      <MenuItem key={item.id} value={item.id}>
+        {item.name}
       </MenuItem>
     );
   });
@@ -31,47 +41,73 @@ function menuItem(map: Map<number, string>): JSX.Element[] {
 
 function DealSelector({
   clients,
-  deals,
-  onClientChange,
-  onDealChange,
+  onSelectionChange,
 }: DealSelectorProps): JSX.Element {
-  const [clientId, setClientId] = useState<number | "">("");
-  const [dealId, setDealId] = useState<number | "">("");
+  const [selection, setSelection] = useState<Selection>({
+    clientId: "",
+    dealId: "",
+  });
 
-  const handleStateChange = (
-    handlers: ((id: number | "") => void)[],
-    name: string
-  ): ((e: SelectChangeEvent<number>) => void) => {
-    return (e: SelectChangeEvent<number>) => {
-      console.log("handleStateChange", name);
-      for (const h of handlers) {
-        const v = e.target.value;
-        const id = typeof v === "string" ? maybeInt(v) : v;
-        h(id ?? "");
+  const deals = useMemo(() => {
+    if (typeof selection.clientId === "number") {
+      const client = clients.find((c) => c.id === selection.clientId);
+      if (client === undefined) {
+        throw new Error("panic");
       }
-    };
-  };
-
-  const clearDealId = () => {
-    setDealId("");
-    onDealChange("");
-  };
-  console.log("deal selector");
+      return client.deals;
+    } else {
+      return clients.map((c) => c.deals).flat();
+    }
+  }, [selection.clientId]);
 
   return (
     <Box style={{ marginLeft: "10px" }}>
       <Select
-        onChange={handleStateChange(
-          [onClientChange, setClientId, clearDealId],
-          "client change"
-        )}
-        value={clientId}
+        onChange={(e) => {
+          const v = e.target.value;
+          const id = typeof v === "string" ? "" : v;
+          const newSelection: Selection = {
+            clientId: id,
+            dealId: "",
+          };
+          setSelection(newSelection);
+          onSelectionChange(newSelection);
+        }}
+        value={selection.clientId}
       >
         {menuItem(clients)}
       </Select>
       <Select
-        onChange={handleStateChange([onDealChange, setDealId], "deal change")}
-        value={dealId}
+        onChange={(e) => {
+          const v = e.target.value;
+          const id = typeof v === "string" ? "" : v;
+          if (id === "") {
+            const newSelection: Selection = {
+              ...selection,
+              dealId: "",
+            };
+            setSelection(newSelection);
+            onSelectionChange(newSelection);
+          } else {
+            const clientId = (() => {
+              for (const c of clients) {
+                for (const d of c.deals) {
+                  if (d.id === id) {
+                    return c.id;
+                  }
+                }
+              }
+              throw new Error("panic");
+            })();
+            const newSelection: Selection = {
+              clientId: clientId,
+              dealId: id,
+            };
+            setSelection(newSelection);
+            onSelectionChange(newSelection);
+          }
+        }}
+        value={selection.dealId}
       >
         {menuItem(deals)}
       </Select>
